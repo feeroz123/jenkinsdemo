dbf2aa92b4e0417df818291856b4c8931c62fb99
Feeroz Alam <feeroz123@gmail.com>
2018-07-17 20:17:15 +0530

    Added first program

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+* This is a sample javadoc
+*@author 	Feeroz
+*@version	1.0
+*/
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// This is my first program
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Hello world");
+		}
+		
+	}
+
+}
